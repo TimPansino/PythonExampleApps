04ec776925dc09884a9947aa894ec508f75d28aa
Tim Pansino <timpansino@gmail.com>
2021-08-17 11:08:58 -0700

    Formatting

--- FastAPIDemoApp/routers/routes.py
+++ FastAPIDemoApp/routers/routes.py
@@ -5,13 +5,16 @@ from fastapi import APIRouter, BackgroundTasks, Request
 
 router = APIRouter()
 
+
 @router.get("/")
 def index(request: Request, background_tasks: BackgroundTasks):
     return "Hello from FastAPI!"
 
+
 @router.get("/async")
 async def index_async(request: Request, background_tasks: BackgroundTasks):
     return "Hello from FastAPI!"
+
 
 @router.get("/error")
 async def index_async(request: Request, background_tasks: BackgroundTasks):
