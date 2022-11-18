ec94d744ce800935b68ea38e28ddb806be8f35b2
Tim Pansino <timpansino@gmail.com>
2022-11-18 12:27:39 -0800

    Added index template

--- DjangoChannelsDemo/channelsdemo/settings.py
+++ DjangoChannelsDemo/channelsdemo/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'daphne',
     "chatsync",
     "chatasync",
+    "channelsdemo",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- DjangoChannelsDemo/channelsdemo/views.py
+++ DjangoChannelsDemo/channelsdemo/views.py
@@ -1,31 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-from textwrap import dedent
+# Create your views here.
 
 def index(request):
     absolute_url = request.build_absolute_uri().rstrip("/")
-
-    text = dedent(f"""
-    <body>
-        <p>
-            This is a Django Channels demo adapted from https://channels.readthedocs.io/en/latest/tutorial/index.html
-        </p>
-        <p>
-            To connect using websockets, proceed to
-        </p>
-        <p>
-            <a href="{absolute_url}/chatasync/">Async Chatrooms</a>
-        </p>
-        <p>
-            or
-        </p>
-        <p>
-            <a href="{absolute_url}/chatsync/">Sync Chatrooms</a>
-        </p>
-        <p>
-            Both sync and async chatrooms can freely communicate with each other, provided you use the same chat room name.
-        </p>
-    </body>
-    """).strip()
-
-    return HttpResponse(text, content_type="text/html")
+    return render(request, "channelsdemo/index.html", {"absolute_url": absolute_url})
